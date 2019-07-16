b2132c12ff2826111e3f22406bf2ccfa0810a76c
bernouilli <riya.ranjan.00@gmail.com>
2019-07-16 09:50:39 -0700

    Committing high_five.py

--- multi-party-interaction-master/Riya_Ishaan_Code/high_five.py
+++ multi-party-interaction-master/Riya_Ishaan_Code/high_five.py
@@ -13,7 +13,7 @@ def main(IP):
     motion = ALProxy("ALMotion", IP, PORT)
     motion.setStiffnesses("RArm", 1.0) #stiffness must be >1 for robot to move
     shoulder = "RShoulderPitch"
-    shoulderAngle = -2.0857
+    shoulderAngle = -1.0
     fractionMaxSpeedShoulder = 0.1
     motion.setAngles(shoulder, shoulderAngle, fractionMaxSpeedShoulder)
 
@@ -21,6 +21,9 @@ def main(IP):
 
     tts = ALProxy("ALTextToSpeech", IP, 9559)
     tts.say("High five!")
+
+    time.sleep(3.0)
+    motion.setAngles(shoulder, 1.5, fractionMaxSpeedShoulder)
 
 if __name__=="__main__":
      main(IP)
