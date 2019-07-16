b019f9c445c9a0a2da04673f571d7c885863f3a4
IshaanChandra <ishaanc3@gmail.com>
2019-07-16 14:30:28 -0700

    fix dance and final updates on touch hand

--- multi-party-interaction-master/Riya_Ishaan_Code/Gestures/hands_together.py
+++ multi-party-interaction-master/Riya_Ishaan_Code/Gestures/hands_together.py
@@ -48,14 +48,17 @@ def main(IP):
     motion.setAngles(wristYaw, wristYawAngle, fractionMaxSpeedWristYaw)
 
 def config_beginning():
+   
+    shoulderRollBegin = ["LShoulderRoll", "RShoulderRoll"]
+    angleShoulderRoll = [0.5,0.5]
+    motion.setAngles(shoulderRollBegin, angleShoulderRoll, fractionMaxSpeed)
+    time.sleep(1.0)
+
     shoulderPitchBegin = ["LShoulderPitch", "RShoulderPitch"]
     angleShoulderPitch = [1.5, 1.5]
     fractionMaxSpeed = 0.1
     motion.setAngles(shoulderPitchBegin,angleShoulderPitch,fractionMaxSpeed)
 
-    shoulderRollBegin = ["LShoulderRoll", "RShoulderRoll"]
-    angleShoulderRoll = [0.5,0.5]
-    motion.setAngles(shoulderRollBegin, angleShoulderRoll, fractionMaxSpeed)
 
     elbowYawBegin = ["LElbowYaw", "RElbowYaw"]
     angleElbowYaw = [0,0]
