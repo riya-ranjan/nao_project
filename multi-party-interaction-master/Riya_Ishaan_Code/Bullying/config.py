dd1377f6db784b904ce03f0ca5e15974e3b03bd3
bernouilli <riya.ranjan.00@gmail.com>
2019-07-16 10:30:24 -0700

    Adding Config part 2

---
+++ multi-party-interaction-master/Riya_Ishaan_Code/Bullying/config.py
@@ -0,0 +1,25 @@
+import os.path
+
+
+ROBOT_IP = "192.168.6.166"
+ROBOT_PORT = 9559
+
+SERVER_HOST = "127.0.0.1"
+SERVER_PORT = 5678
+
+
+def get_project_root():
+    current_filepath = os.path.abspath(__file__)
+
+    return os.path.dirname(current_filepath)
+
+
+def to_path_str(curr_path, *path_parts):
+    return os.path.abspath(os.path.join(curr_path, *path_parts))
+
+
+# project root
+projectRootPath = get_project_root()
+
+debugScriptPath = to_path_str(projectRootPath, "debug_scripts")
+
