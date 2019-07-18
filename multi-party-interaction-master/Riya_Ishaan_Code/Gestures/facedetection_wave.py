e0f35c24c471203c0945233b0f23d0c9ba34b1dd
riya-ranjan <riya.ranjan.00@gmail.com>
2019-07-17 18:58:29 -0700

    Waving hand added to face detection :)

--- multi-party-interaction-master/Riya_Ishaan_Code/Gestures/facedetection_wave.py
+++ multi-party-interaction-master/Riya_Ishaan_Code/Gestures/facedetection_wave.py
@@ -7,30 +7,6 @@ import config
 IP = config.ROBOT_IP
 PORT = config.ROBOT_PORT
 
-def waveHand():
-        motion = ALProxy("ALMotion", IP, PORT)
-        motion.setStiffnesses("RArm", 1.0) #stiffness must be >1 for robot to move
-        shoulder = "RShoulderPitch"
-        shoulderAngle = -0.75
-        ractionMaxSpeedShoulder = 0.1
-        motion.setAngles(shoulder, shoulderAngle, fractionMaxSpeedShoulder)
-
-        motion.openHand("RHand")
-
-        elbowYaw = "RElbowYaw"
-        elbowYawAngle =  0.0
-        fractionMaxSpeedElbow = 0.1
-        motion.setAngles(elbowYaw, elbowYawAngle, fractionMaxSpeedElbow)
-    
-        time.sleep(2)
-
-        #Option 1: wave with elbow movement
-        elbowRoll = "RElbowRoll"
-        angleLists = [1, 0.5, 0.03, 0.5, 1, 0.5, 0.03]
-        times = [1, 1.8, 2.2, 2.6, 3.0, 3.4, 3.8]
-        isAbsolute = True
-        motion.angleInterpolation(elbowRoll, angleLists, times, isAbsolute)
-
 class HumanGreeter(object):
     """
     A simple class to react to face detection events.
@@ -54,6 +30,29 @@ class HumanGreeter(object):
         self.face_detection.subscribe("HumanGreeter")
         self.got_face = False
 
+    def waveHand():
+        motion = ALProxy("ALMotion", IP, PORT)
+        motion.setStiffnesses("RArm", 1.0) #stiffness must be >1 for robot to move
+        shoulder = "RShoulderPitch"
+        shoulderAngle = -0.75
+        ractionMaxSpeedShoulder = 0.1
+        motion.setAngles(shoulder, shoulderAngle, fractionMaxSpeedShoulder)
+
+        motion.openHand("RHand")
+
+        elbowYaw = "RElbowYaw"
+        elbowYawAngle =  0.0
+        fractionMaxSpeedElbow = 0.1
+        motion.setAngles(elbowYaw, elbowYawAngle, fractionMaxSpeedElbow)
+    
+        time.sleep(2)
+
+        #Option 1: wave with elbow movement
+        elbowRoll = "RElbowRoll"
+        angleLists = [1, 0.5, 0.03, 0.5, 1, 0.5, 0.03]
+        times = [1, 1.8, 2.2, 2.6, 3.0, 3.4, 3.8]
+        isAbsolute = True
+        motion.angleInterpolation(elbowRoll, angleLists, times, isAbsolute)
         
     def on_human_tracked(self, value):
         """
